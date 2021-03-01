e0a2920dee865a92134457e930e45c3d09549f62
Alejandro Vera <alejandroverah97@gmail.com>
2021-03-01 14:14:25 -0500

    generando una secuencia de numeros

--- SimonDice/codigo.js
+++ SimonDice/codigo.js
@@ -5,16 +5,33 @@ const verde = document.getElementById("verde");
 const btnEmpezar = document.getElementById("btnEmpezar");
 
 class Juego {
-constructor() {
-    this.inicializar();
-}
+    constructor() {
+        this.inicializar();
+        this.generarSecuencia();
+    }
 
-inicializar() {
-    //Lo primero que hace es ocultar el boton empezar
-    btnEmpezar.classList.add("hide");
-}
+    inicializar() {
+        //Lo primero que hace es ocultar el boton empezar
+        btnEmpezar.classList.add("hide");
+        this.nivel= 1;
+        this.colores = {
+            celeste,
+            violeta,
+            naranja,
+            verde
+        }
+    }
+
+    generarSecuencia() {
+        //generar un array de numeros random
+        //this.secuencia=[1,2,3,4,5] esta es una forma
+        //map no va a funcionar cuando tengamos elementos sin definir en el array
+        //math.floor redondea para abajo un numero
+        this.secuencia=new Array(10).fill(0).map(n => Math.floor(Math.random() * 4));
+    }
+
 }
 
 function empezarJuego() {
-var juego = new Juego();
+window.juego = new Juego();
 }
